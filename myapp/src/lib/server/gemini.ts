671e5745f4133a9eba496e5a0e9fafc1e4bcebae
Kenneth <kokiidoki@gmail.com>
2025-10-25 16:07:29 -0400

    get response from gemini ;D

---
+++ myapp/src/lib/server/gemini.ts
@@ -0,0 +1,35 @@
+import { GoogleGenAI, Type } from "@google/genai";
+
+// KENNY'S API KEY FOR FREE PLAN
+const ai = new GoogleGenAI({apiKey: "AIzaSyA_yIH40hmV07f5BFLTmczZlyYEarjsFLw"});
+
+export async function extract_data_from_syllabi(syllabi: string[]) {
+
+    const prompt = [
+        { text: "Extract the important due dates from these class syllabi, and write them into an ics file as a string." },
+    ];
+
+    for (let i in syllabi) {
+        prompt.push({
+            inlineData: {
+                mimeType: 'application/pdf',
+                data: syllabi[i]
+            }
+        });
+    }
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseSchema: {
+                type: Type.ARRAY,
+                items: {
+                    type: Type.STRING
+                },
+            }
+        }
+    });
+    console.log(response.text);
+}
---
+++ myapp/src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { Buffer } from 'node:buffer';
+import * as gemini from "$lib/server/gemini.ts";
+
+export const actions = {
+    // ({property}) is js destructuring syntax
+    default: async ({request}) => {
+        const form_file_input_name = "file";
+        const form_data_list = (await request.formData()).getAll(form_file_input_name);
+        const files = [];
+        for (let i = 0; i < form_data_list.length; i++) {
+            const maybe_file = form_data_list[i];
+            if (maybe_file instanceof File) {
+                files[i] = Buffer.from(await maybe_file.arrayBuffer()).toString("base64");
+                
+            }
+        }
+
+        gemini.extract_data_from_syllabi(files);
+    }
+}
