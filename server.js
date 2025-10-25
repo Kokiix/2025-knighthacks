74d9dab18308f53b5b16cd78eb7d189fe47d3293
Kenneth <kokiidoki@gmail.com>
2025-10-25 12:12:26 -0400

    abort idea

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
-app.get('/abc', async (req, res) => {
-    return res.status(400).send('URL is required');
-});
+let testabc;
 
-app.listen(3001, () => {
-  console.log(`Proxy server is running on port}`);
-});
+async function test() {
+    const resp1 = await axios.get("https://www.instagram.com/");
+    const resp = await axios.get("https://www.instagram.com/");
+    testabc = resp.data;
+
+    app.get('/', (req, res) => {
+        // res.set({
+        //     'Content-Type': 'text/html',
+        //     'Content-Disposition': 'attachment; filename="my-file.html"'
+        // });
+
+        // Send the HTML string as the response
+        console.log(testabc);
+        res.send(testabc);
+        res.send("hello world");
+    })
+
+    app.listen(3001, () => {
+    console.log(`Proxy server is running on port}`);
+    });
+}
+
+test();
